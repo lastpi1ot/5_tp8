1cd7e205ad58e4cc45348c808fbba5fecfb31db6
lastpi1ot <hamzaett1320@gmail.com>
2025-10-31 21:14:30 +0100

    tp8

---
+++ src/main/java/ma/rest/spring/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package ma.rest.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
+
